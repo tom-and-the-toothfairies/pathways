4aab2636d888c9776ea5e145534ca64dca152563
shawa <andrewshaw.mail@gmail.com>
2017-01-31 14:09:37 +0000

    Add loading of DINTO from .owl file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import clize
 import owlready
 
-def main():
-    pass
+def main(dinto_file):
+    """Perform pathway analysis given some Drug Interaction Ontology.
+
+    dinto_file: path to DINTO ontology OWL file
+
+    """
+
+    owlready.get_ontology(dinto_file)
 
 
 if __name__ == '__main__':
