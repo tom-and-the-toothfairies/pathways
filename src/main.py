e7e282d2b8ce7d4843cfbcdc1df7c60b2cfa910e
shawa <andrewshaw.mail@gmail.com>
2017-01-31 16:54:41 +0000

    Break load_graph into own function
    (It's handier for iPython)

--- src/main.py
+++ src/main.py
@@ -4,21 +4,29 @@ import logging
 from itertools import islice
 
 
+def load_from_file(dinto_file):
+    """
+    return a loaded Graph() object from the given `dinto_file`
+    """
+    g = rdflib.Graph()
+    logging.info('Created graph')
+    g.load(dinto_file)
+    logging.info('Finished Loading {}'.format(dinto_file))
+    return g
+
+
 def main(dinto_file):
     """Perform pathway analysis given some Drug Interaction Ontology.
 
     dinto_file: path to DINTO ontology OWL file
 
     """
-    g = rdflib.Graph()
-    logging.info('Created graph')
-    g.load(dinto_file)
-    logging.info('Finished Loading {}'.format(dinto_file))
-
+    g = load_from_file(dinto_file)
     for s, p, o in islice(g, 10):
         print(f'Subject: {s}')
         print(f'Predicate: {p}')
         print(f'Object: {o}')
+
 
 if __name__ == '__main__':
     clize.run(main)
