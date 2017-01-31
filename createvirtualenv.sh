67a0e1140f7584edf12ed6d5727929dfa08c29ef
shawa <andrewshaw.mail@gmail.com>
2017-01-31 14:00:58 +0000

    Add requirements.txt, venv script

---
+++ createvirtualenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm -rf env
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import clize
+import owlready
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    clize.run(main)
+
