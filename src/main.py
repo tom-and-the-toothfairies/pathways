86a77a058542ad264e9e9b8195c7d1920b1c5eb5
Andrew Shaw <shawa@users.noreply.github.com>
2017-02-20 10:35:24 +0000

    Add a flask app that returns all of the Drugs it can find, and all of the DDIs it can find.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,17 @@
-import clize
-import rdflib
-import logging
-import os
+from flask import Flask, jsonify
+from dinto import dinto
+app = Flask(__name__)
 
 
-def load_from_file(dinto_file):
-    """
-    return a loaded Graph() object from the given `dinto_file`
-    """
-    g = rdflib.Graph()
-    logging.info('Created graph')
-    g.load(dinto_file)
-    logging.info('Finished Loading {}'.format(dinto_file))
-    return g
+@app.route("/drugs")
+def drugs():
+    return jsonify(dinto.all_drugs)
 
 
-def main(dinto_file):
-    """Count the number of triples in the given ontology
-
-    dinto_file: path to DINTO ontology OWL file
-
-    """
-    g = load_from_file(dinto_file)
-
-    print("Ontology {} contains {} triples"
-          .format(os.path.basename(dinto_file), len(g)))
+@app.route("/ddis")
+def ddis():
+    return jsonify(dinto.all_ddis)
 
 
 if __name__ == '__main__':
-    clize.run(main)
+    app.run(debug=True)
