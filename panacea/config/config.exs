134b0519e49a675281c9d98d6d4d2c00bc122401
Eoin Houlihan <ehoulih@tcd.ie>
2017-02-21 19:15:57 +0000

    JUnit XML directory change

--- panacea/config/config.exs
+++ panacea/config/config.exs
@@ -18,10 +18,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
-
 config :panacea, :asclepius,
   host: "asclepius",
   port: 5000
+
+config :junit_formatter,
+  report_dir: "/test-reports"
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
