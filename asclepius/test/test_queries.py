81304f64ac91dc6f7825dd117780baa90c46682a
Eoin Houlihan <ehoulih@tcd.ie>
2017-03-08 21:32:17 +0000

    Fix python tests

--- asclepius/test/test_queries.py
+++ asclepius/test/test_queries.py
@@ -1,14 +1,23 @@
 import pytest
 
 from asclepius.dinto import (
-        ddi_from_drugs,
-    )
+    ddi_from_drugs,
+    drugs
+)
 
 
 def test_ddi_from_drugs_raises_with_uncachable_things():
-    with pytest.raises(ValueError):
+    with pytest.raises(TypeError):
         ddi_from_drugs([''])
 
-def test_ddi_from_drugs_raises_with_too_few_args():
+def test_ddi_from_drugs_raises_with_too_few_drugs():
     with pytest.raises(ValueError):
         ddi_from_drugs(frozenset(['http://perl.oboelibrary.org/din-toe/chubby:1245']))
+
+def test_ddi_from_drugs_raises_with_non_iterable():
+    with pytest.raises(TypeError):
+        ddi_from_drugs(object())
+
+def test_drugs_raises_with_non_iterable():
+    with pytest.raises(TypeError):
+        drugs(object())
