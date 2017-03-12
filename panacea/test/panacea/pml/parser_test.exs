6962662f5657b21fbc2770357d8ccae8989436a9
c-brenn <brennc27@tcd.ie>
2017-03-12 18:42:10 +0000

    removes tags from panacea tests
    We used the tags to allow the client to run specific tests as part of his
    acceptance testing. We now have a suite of end-to-end tests that remove the need
    for these tags

--- panacea/test/panacea/pml/parser_test.exs
+++ panacea/test/panacea/pml/parser_test.exs
@@ -6,7 +6,6 @@ defmodule Panacea.Pml.ParserTest do
   @fixtures_dir Path.join(~w(#{@root_dir} test fixtures jnolls_pml))
 
   describe "parse/1" do
-    @tag :pml_analysis
     test "it parses correct pml" do
       pml = """
       process foo {
@@ -25,7 +24,6 @@ defmodule Panacea.Pml.ParserTest do
       assert Parser.parse(pml) == {:ok, []}
     end
 
-    @tag :pml_analysis
     test "it can parse all of jnoll's sample pml" do
       {:ok, files} = File.ls(@fixtures_dir)
       for file <-  files do
@@ -36,8 +34,6 @@ defmodule Panacea.Pml.ParserTest do
       end
     end
 
-    @tag :err_highlights
-    @tag :pml_analysis
     test "it rejects incorrect pml" do
       pml = """
       process foo {{
@@ -46,8 +42,6 @@ defmodule Panacea.Pml.ParserTest do
       assert Parser.parse(pml) == {:error,  {:syntax_error, "line 1 -- syntax error before: '{'"}}
     end
 
-    @tag :identify_drugs
-    @tag :pml_analysis
     test "it identifies drugs" do
       pml = """
         process foo {
