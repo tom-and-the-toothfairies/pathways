caabe393bbae8290212b824d2df1f4533420f149
Conor Brennan <brennc27@tcd.ie>
2017-04-02 18:51:31 +0100

    dead code removal
    * removes unused images
    * removes unused test modules
    * turns on gzipping for assets in production

--- panacea/lib/panacea/endpoint.ex
+++ panacea/lib/panacea/endpoint.ex
@@ -6,7 +6,7 @@ defmodule Panacea.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :panacea, gzip: false,
+    at: "/", from: :panacea, gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
